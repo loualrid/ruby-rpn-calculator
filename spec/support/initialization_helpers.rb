c5e9411bad1ef72df1b963dd793df38d6b4f36dc
Louis Alridge <loualrid@gmail.com>
2018-01-03 23:10:18 -0800

    Fix failing test from rubocop change

--- spec/support/initialization_helpers.rb
+++ spec/support/initialization_helpers.rb
@@ -2,7 +2,7 @@
 
 module InitializationSpecHelpers
   def initialize_and_set_stdin_to(string_array = [], run_modes_array = [])
-    if string_array.empty?
+    unless string_array.empty?
       allow($stdin).to receive(:gets).and_return(*string_array)
     end
 
